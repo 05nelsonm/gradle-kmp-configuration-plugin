d009eefb614f5ae1866548f6cc22c0b974ade277
Matthew Nelson <developer@matthewnelson.io>
2023-02-10 05:26:11 -0500

    Clean up code (#7)

--- plugin/src/main/kotlin/io/matthewnelson/kmp/configuration/extension/container/target/TargetWasmNativeContainer.kt
+++ plugin/src/main/kotlin/io/matthewnelson/kmp/configuration/extension/container/target/TargetWasmNativeContainer.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-
 @file:Suppress("ClassName")
 
 package io.matthewnelson.kmp.configuration.extension.container.target
