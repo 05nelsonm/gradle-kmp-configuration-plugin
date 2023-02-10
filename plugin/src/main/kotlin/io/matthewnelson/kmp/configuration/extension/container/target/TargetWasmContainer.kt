827df1b80ef1b014c26439d93bb8514f259c7638
Matthew Nelson <developer@matthewnelson.io>
2023-02-10 07:24:57 -0500

    Add default `wasm` setup for no action invocation (#12)

--- plugin/src/main/kotlin/io/matthewnelson/kmp/configuration/extension/container/target/TargetWasmContainer.kt
+++ plugin/src/main/kotlin/io/matthewnelson/kmp/configuration/extension/container/target/TargetWasmContainer.kt
@@ -32,7 +32,27 @@ public class TargetWasmContainer internal constructor(
         public val holder: ContainerHolder
 
         @ExperimentalWasmDsl
-        public fun wasm() { wasm {} }
+        public fun wasm() {
+            wasm { container ->
+                container.target { dsl ->
+                    dsl.browser {
+                        testTask {
+                            useMocha { timeout = "30s" }
+                        }
+                    }
+                    dsl.nodejs {
+                        testTask {
+                            useMocha { timeout = "30s" }
+                        }
+                    }
+                    dsl.d8 {
+                        testTask {
+                            useMocha { timeout = "30s" }
+                        }
+                    }
+                }
+            }
+        }
 
         @ExperimentalWasmDsl
         public fun wasm(action: Action<TargetWasmContainer>) {
