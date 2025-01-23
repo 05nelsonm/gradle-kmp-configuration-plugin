13c8c2458c623e87648786e89d681085a4b688a8
Matthew Nelson <developer@matthewnelson.io>
2025-01-23 06:41:05 -0500

    Update dependencies to latest stable versions (#61)

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 
 dependencies {
     implementation(libs.gradle.kotlin)
-    implementation(libs.gradle.maven.publish)
+    implementation(libs.gradle.publish.maven)
 
     // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
